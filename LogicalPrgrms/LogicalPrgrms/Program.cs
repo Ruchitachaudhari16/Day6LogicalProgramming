628282345b85cbf2c6cd5c05e54009959e13104b
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-06-20 23:39:18 -0400

    [Ruchita] Add: Reverse Number

--- LogicalPrgrms/LogicalPrgrms/Program.cs
+++ LogicalPrgrms/LogicalPrgrms/Program.cs
@@ -14,6 +14,7 @@ namespace LogicalPrgrms
             Console.WriteLine("Enter the choice ");
             Console.WriteLine("1:Fibonacci Series");
             Console.WriteLine("2:Prime Number");
+            Console.WriteLine("3:Reverse Number");
             int choice=Convert.ToInt32(Console.ReadLine());
             switch (choice)
             {
@@ -27,6 +28,12 @@ namespace LogicalPrgrms
                     Console.WriteLine("Prime Number");
                     PrimeNumber primeNumber = new PrimeNumber();
                     primeNumber.primeCheck();
+                    break;
+
+                    case 3:
+                    Console.WriteLine("Reverse Number");
+                    Reverse reverse = new Reverse();
+                    reverse.checkReverse();
                     break;
             }
             Console.ReadLine();
---
+++ LogicalPrgrms/LogicalPrgrms/Reverse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrgrms
+{
+    public class Reverse
+    { 
+    public void checkReverse()
+    {
+        int number, reverse = 0, remainder;
+        Console.Write("Enter a number: ");
+           number = int.Parse(Console.ReadLine());
+            while (number != 0)
+            {
+                remainder = number % 10;
+                reverse = reverse*10 + remainder;
+                number =number/10;
+            }
+    Console.Write("Reversed Number: " + reverse);
+        }
+    }
+
+    }
+
