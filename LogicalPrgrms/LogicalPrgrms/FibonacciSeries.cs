71615bb26c65082a0ad50a1d5260c9af07363b09
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-06-20 22:34:27 -0400

    [Ruchita] Add: FibonacciSeries

---
+++ LogicalPrgrms/LogicalPrgrms/FibonacciSeries.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrgrms
+{
+    public class FibonacciSeries
+    {
+        //FibonacciSeries.
+        public void getFibonacciSeries()
+        {
+            int a = 0;
+            int b = 1;
+            int c;
+            Console.WriteLine("Enter the number to print fibonacci Series");
+             int number=Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            for (int i = 0; i <number; i++)
+            {
+                c = a + b;
+                Console.WriteLine(c);
+                a = b;
+                b=c;
+
+            }
+            
+
+            
+        }
+    }
+}
---
+++ LogicalPrgrms/LogicalPrgrms/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrgrms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Logical Programming");
+            Console.WriteLine("Enter the choice ");
+            int choice=Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Fibonacci Series");
+                    FibonacciSeries series = new FibonacciSeries();
+                    series.getFibonacciSeries();
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
